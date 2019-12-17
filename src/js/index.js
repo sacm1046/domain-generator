21d96925783fdf5f9e1eeecc5eecd48672f66cd4
Sebastian Contreras <sacm1046@gmail.com>
2019-12-17 14:07:28 -0300

    Domain Generator

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,18 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var options = [];
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  for (let x = 0; x < pronoun.length; x++) {
+    for (let y = 0; y < adj.length; y++) {
+      for (let z = 0; z < noun.length; z++) {
+        options.push(" www." + pronoun[x] + adj[y] + noun[z] + ".com");
+        //console.log(options);
+        document.getElementById("myClass").innerHTML = options;
+      }
+    }
+  }
 };
